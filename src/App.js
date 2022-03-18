af62464dc09c7d5fd358fd06950af077598d089d
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-03-18 17:05:14 +1000

    Chat user & chat room created

--- src/App.js
+++ src/App.js
@@ -1,2 +1,18 @@
 import { ChatEngine } from "react-chat-engine";
 import './App.css'
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <ChatEngine 
+        height="100vh"
+        projectID="f3f2d93a-5c8f-46cd-b097-11194075ec24"
+        userName="javascriptmastery"
+        userSecret="123123"
+    />
+
+  )
+}
+
+export default App
